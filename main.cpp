156f9541a21a11ad7dace582de460effa4e20097
Nircek <marcinek0302@gmail.com>
2017-03-24 22:04:21 +0100

    add prototypes

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
+#include "qtppsps.h"
+using namespace GitHub::Nircek;
 int main(int argc, char *argv[])
 {
+    QApplication qapp(argc,argv);
+    qtppsps ps("localhost", "super tajne hasło", &qapp);
     return 0;
 }
 
--- qtppsps.cpp
+++ qtppsps.cpp
@@ -1,6 +1,44 @@
 #include "qtppsps.h"
+using namespace GitHub::Nircek;
+qtppsps::qtppsps(QString host, QString pass, QObject *parent):
+    host(host),pass(pass),parent(parent){
 
-qtppsps::qtppsps()
-{
+}
 
+qtppsps::~qtppsps(){
+
+}
+QString GitHub::Nircek::readPPSReplyType(PPSReplyType type){
+    switch(type){
+    case good:          return "good";
+    case paramerror:    return "paramERR";
+    case dberror:       return "dbERR";
+    case httperror:     return "httpERR";
+    case error:         return "unexceptedERR";
+    }
+    return "impossibleERR";
+}
+
+PPSReply::PPSReply(char c, QString s):
+    reply(s){
+    switch(c){
+    case '0':   construct(good,s);          break;
+    case '-':   construct(paramerror,s);    break;
+    case '+':   construct(dberror,s);       break;
+    case '/':   construct(httperror,s);     break;
+    default:    construct(error,s);         break;
+    }
+}
+
+PPSReply::PPSReply(PPSReplyType type, QString s){
+    construct(type,s);
+}
+
+void PPSReply::construct(PPSReplyType type, QString s){
+    replyType=type;
+    reply=s;
+}
+
+PPSReply qtppsps::toReply(QString s){
+    return PPSReply(s.mid(0,1).toLatin1()[0],s.mid(1));
 }
